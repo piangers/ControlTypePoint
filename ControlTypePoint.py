1fd9cca961dd559c3d9f82feda0b2b393998985b
Cesar Piangers <cesarpiangers@gmail.com>
2018-10-17 16:41:29 -0300

    inicial

--- ControlTypePoint.py
+++ ControlTypePoint.py
@@ -1,5 +1,6 @@
 # -*- coding: UTF-8 -*-
 from qgis.core import *
+from qgis.gui import *
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from selectiontool import SelectionTool
@@ -7,7 +8,12 @@ import resources_rc
 import math
 
 
-class ControlPoint:
+#Carrega o arquivo da interface .ui
+sys.path.append(os.path.dirname(__file__))
+GUI,_= uic.loadUiType (os.path.join(os.path.dirname(__file__),'ui','dlg.ui'),resource_suffix='')
+
+class ControlTypePoint( GUI ):
+
 
     def __init__(self, iface):
       # Save reference to the QGIS interface
@@ -18,14 +24,15 @@ class ControlPoint:
     def initGui(self):
         # 1 - CRIAR UM BOTÃO PARA ATIVAR A FERRAMENTA
         settings = QSettings()
-        self.tAction = QAction(QIcon(":/plugins/ControlPoint/tr.png"), u'Trim', self.iface.mainWindow())
+        self.tAction = QAction(QIcon(":/plugins/ControlTypePoint/tr.png"), u'Control tools', self.iface.mainWindow())
         self.tAction.setCheckable(True)
         self.spinBox = QDoubleSpinBox(self.iface.mainWindow())
-        self.toolbar = self.iface.addToolBar(u'Trim tools')
+        self.toolbar = self.iface.addToolBar(u'Control tools')
         
         # 2 - CONECTAR O CLIQUE DO BOTÃO COM UM MÉTODO ("SLOT")
         self.tAction.toggled.connect(self.run)
-        self.spinBox.valueChanged.connect(self.setTolerancia)
+        self.spinBox.valueChanged.connect(self.setRaio)
+        self.comboBox.currentIndexChanged.connect(self.layer_field)
        
         #Padrões fixados
         
@@ -33,20 +40,24 @@ class ControlPoint:
         self.spinBox.setMinimum(0.000)
         self.spinBox.setMaximum(5000.000)
         self.spinBox.setSingleStep(0.100)
-        self.tolerancia = self.spinBox.value()
-        self.spinBox.setToolTip("Escala de avaliacao")
+        self.raio = self.spinBox.value()
+        self.spinBox.setToolTip("Raio de busca de Pontos.")
         self.toolbar.addAction(self.tAction)
-        self.combobox = QComboBox() # cria o objeto combobox:
+        
 
     def unload(self):
         del self.toolbar
         
-    def setTolerancia(self, t):
-        self.tolerancia = t # recebendo tolerância atravéz de t.
+    def setRaio(self, t):
+        self.raio = t # recebendo Raio atravéz de t.
 
+    
     def run(self):
         """Run method that performs all the real work"""
-       
+
+        # cria o objeto combobox:
+        self.dlg.comboBox = QComboBox() 
+        
         # Obter todas as camadas carregadas na interface
         layers = self.iface.legendInterface().layers()
         self.layers = [layer for layer in self.iface.legendInterface().layers() if layer.type() == QgsMapLayer.VectorLayer]
@@ -60,16 +71,15 @@ class ControlPoint:
             layer_list.append(layer.name())
 
         # Clear comboBox (útil para não criar itens duplicados na lista)
-        self.comboBox.clear()
+        self.dlg.comboBox.clear()
    
-        # Adicione todos os itens na lista ao comboBox
-        self.comboBox.addItems()
-        self.comboBox.resize(200,35)
+        # Adicione todos os itens na lista ao comboBox por nome
+        self.dlg.comboBox.addItems(layer_list)
+        
         
         # exibe o diálogo
-        self.comboBox.show()
+        self.dlg.comboBox.show()
         
-    def layer_field(self):
 
         # Identifique a camada selecionada pelo seu índice
         selectedLayerIndex = self.dlg.comboBox.currentIndex()
@@ -79,28 +89,29 @@ class ControlPoint:
         fields = selectedLayer.pendingFields()
 
         # Obter nomes de campos dos campos
-        fieldnames = [field.name() for field in fields]
+        # fieldnames = [field.name() for field in fields]
 
         # Clear comboBox_5
-        self.dlg.comboBox_5.clear()
+        self.dlg.comboBox_2.clear()
 
         # Adicione nomes de campos ao comboBox_5
-        self.dlg.comboBox_5.addItems()
+        self.dlg.comboBox_2.addItems(layer_list)
 
         # Mesmos comentários como acima
         selectedLayerIndex = self.dlg.comboBox_2.currentIndex()
         selectedLayer = self.layers[selectedLayerIndex]
         fields = selectedLayer.pendingFields()
         fieldnames = [field.name() for field in fields]
-        self.dlg.comboBox_3.clear()            
-        self.dlg.comboBox_3.addItems(fieldnames)
+        self.dlg.comboBox_2.clear()            
+        self.dlg.comboBox_2.addItems("1:250.000","1:100.000","1:50.000","1:25.000","1:10.000","1:2.000","1:1000")
         self.dlg.comboBox_4.clear()
         self.dlg.comboBox_4.addItems(fieldnames)
 
     # Ao alterar a camada no comboBox, execute a função "layer_field ()"
     # para atualizar os nomes dos campos nas comboBoxes associadas
-    self.dlg.comboBox.currentIndexChanged.connect()
+    
     
 
     # Para escolher uma camada: QgsMapLayerComboBox
     # Para escolher um campo em uma camada: QgsFieldComboBox
+
